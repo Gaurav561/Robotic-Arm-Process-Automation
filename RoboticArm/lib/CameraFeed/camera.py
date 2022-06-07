739219a3ed8a36aeb9aea0188fcc7266670814e8
Gaurav <gbansal12057@gmail.com>
2022-06-07 22:41:33 +0530

    yolov5 model Integration(Working)

--- RoboticArm/lib/CameraFeed/camera.py
+++ RoboticArm/lib/CameraFeed/camera.py
@@ -1,4 +1,11 @@
 import cv2
+from .object_detection import Model
+import numpy as np
+
+import os
+import io
+import PIL.Image as Image
+import torch
 
 class VideoCamera(object):
     def __init__(self):
@@ -6,6 +13,14 @@ class VideoCamera(object):
         # from a webcam, comment the line below out and use a video file
         # instead.
         self.video = cv2.VideoCapture(0)
+        self.yolo = Model.YoloModel()
+        self.model = self.yolo.load_model()
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+        self.model = self.model.to(self.device)
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('video.mp4')
@@ -15,9 +30,16 @@ class VideoCamera(object):
     
     def get_frame(self):
         success, image = self.video.read()
+        # image = torch.FloatTensor(image)
+        # image = image.to(self.device)
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
+        
+
+        self.yolo.detect(image,self.model)
+
+
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
@@ -25,5 +47,17 @@ class VideoCamera(object):
 def gen(camera):
     while True:
         frame = camera.get_frame()
+
+        # frame = np.array(frame)
+        # image = Image.open(io.BytesIO(bytes))
+
+        # yolo = Model.YoloModel()
+
+        # model = yolo.load_model()
+
+        # yolo.detect(frame,model)
+
+        
+
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
---
+++ RoboticArm/lib/CameraFeed/object_detection/Model.py
@@ -0,0 +1,50 @@
+# from object_detection.yolov5.hubconf import yolov5l
+import torch
+import os
+import cv2
+
+
+class YoloModel:
+
+    def load_model(self):
+        # self,model_name
+        # model_name = os.path.join('object_detection/custom_models/',model_name)
+
+        model = torch.hub.load('/yolov5','custom','yolov5s.pt',source='local',force_reload=True)
+        # model = torch.hub.load('ultralytics/yolov5','yolov5l',force_reload=True )
+        return model
+
+ 
+
+    def detect(self,img,model):
+        result = model(img)
+        labels, cord = result.xyxyn[0][:,
+                                    - 1].to('cpu').numpy(), result.xyxyn[0][:, :-1].to('cpu').numpy()
+        # print(result.pandas().xyxy[0].xmin)
+
+        n = len(labels)
+        #print(n)
+        x_shape, y_shape = img.shape[1], img.shape[0]
+
+
+        for i in range(n):
+            row = cord[i]
+            l = int(labels[i])
+            # lclasses.append(classes.iloc[l, :][0])
+            x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]
+                                                    * y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+    
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 50, 30), 2)
+            # cv2.putText(img, str(classes.iloc[l, :][0]), (x1, y1-10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+
+
+
+
+
+
+
+
+
+    
